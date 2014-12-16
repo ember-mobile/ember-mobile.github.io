85527b5c5466543b2f2469e54c1920a76a7c1a5e
Paul Spieker <p.spieker@duenos.de>
2014-12-16 21:34:15 +0100

    debugging

--- publishToGithub.sh
+++ publishToGithub.sh
@@ -1,16 +1,22 @@
 // publishToGithubPages.sh
 #!/bin/bash
+echo "Building ..."
 git checkout source
+echo "\tcleanup"
 git branch -D master
 git push origin --delete master
 git checkout -b master
+echo "\tbuilding"
 ember build --environment production
+echo "\tcleanup"
 git rm -rf app addon config tests
 git rm -rf Brocfile.js bower.json package.json testem.json
 git rm -rf .bowerrc .editorconfig .jshintrc .travis.yml
 mv dist/* .
 rm -rf dist
+echo "\tpublishing"
 git add .
 git commit -m "Publishing to github pages"
 git push origin master
 git checkout source
+echo "done"
